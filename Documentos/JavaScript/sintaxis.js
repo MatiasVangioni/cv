1b2448a0eff24389eb09519c45b04393e3f42e8c
Matu JK <matiasvangioni@gmail.com>
2023-02-28 22:09:28 -0300

    Delete Documentos directory

--- Documentos/JavaScript/sintaxis.js
+++
@@ -1,7 +0,0 @@
-
-var x;
-let y;
-let Y;
-const z = 1.21;
-
-const IVA= 1.21;
--- Documentos/JavaScript/variables.js
+++
@@ -1,4 +0,0 @@
-
-let longitud = 16;                        // Número
-let apellido = "Johnson";                 // String: Cadena de texto
-let x = {nombre:"Jhon", apellido:"Doe"};  // Objeto
